590204a1e058dddf4a67e4f4981c666de2565c86
Richard C Lim <61879224+richardclim@users.noreply.github.com>
2025-02-01 20:23:37 -0500

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -175,6 +175,16 @@ export const OpenaiPath = {
 
 export const Azure = {
   ChatPath: (deployName: string, apiVersion: string) => {
+      return "chat/completions";
+  },
+  // https://<your_resource_name>.openai.azure.com/openai/deployments/<your_deployment_name>/images/generations?api-version=<api_version>
+  ImagePath: (deployName: string, apiVersion: string) =>
+    `deployments/${deployName}/images/generations?api-version=${apiVersion}`,
+  ExampleEndpoint: "https://{resource-url}",
+}; 
+/*
+export const Azure = {
+  ChatPath: (deployName: string, apiVersion: string) => {
     // Check if it's an AI Foundation endpoint
     if (deployName.includes(".models.ai.azure.com")) {
       return "chat/completions";
@@ -186,7 +196,6 @@ export const Azure = {
     `deployments/${deployName}/images/generations?api-version=${apiVersion}`,
   ExampleEndpoint: "https://{resource-url}",
 }; 
-/* 
 export const Azure = {
   ChatPath: (deployName: string, apiVersion: string) =>
     `deployments/${deployName}/chat/completions?api-version=${apiVersion}`,
