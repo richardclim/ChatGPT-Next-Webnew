247ef187f8fac1109ee1b00459b8945973a964c3
Richard C Lim <61879224+richardclim@users.noreply.github.com>
2025-02-03 21:18:05 -0500

    Update common.ts

--- app/api/common.ts
+++ app/api/common.ts
@@ -19,7 +19,7 @@ const isAzure = req.nextUrl.pathname.includes("azure/deployments") || Boolean(se
       ?.trim()
       .replaceAll("Bearer ", "")
       .trim() ?? ""}`;
-    authHeaderName = "Authorization";
+    authHeaderName = "api-key";
   } else {
     authValue = req.headers.get("Authorization") ?? "";
     authHeaderName = "Authorization";
